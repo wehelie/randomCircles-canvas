1ac808c8fee7d2066ab1b4ea6ba433d7fd6585e1
wehelie <mawehelie@gmail.com>
2015-01-27 21:56:00 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+
+
+function drawCircle() {
+
+	var counter = 500; 
+  		var canvas = document.getElementById("canvas"); 
+		
+		var ctx = canvas.getContext('2d'); 
+
+
+	for (var i = 0; i < counter; i++) {
+
+		var x = Math.random() * 900; 
+		var y = Math.random() * 900; 
+		var radius = Math.floor(Math.random() * 20) + 1; 
+
+		var r = Math.floor(Math.random()* 256);
+		var g = Math.floor(Math.random()* 256);
+		var b = Math.floor(Math.random()* 256);
+
+		var cssColor = 'rgb(' + r +', ' + g + ',' + b +')';
+		var time_begin = new Date(); 
+		ctx.beginPath();
+			
+	
+		ctx.arc(x,y, radius, 0, 2 * Math.PI, true);
+		ctx.fillStyle = cssColor; 
+		ctx.fill();
+		var time_end = new Date(); 
+        var diff = Math.abs( time_begin - time_end);
+
+
+    }
+
+}
+
+	drawCircle(); 
